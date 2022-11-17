aedba3d841601b18e3779740d97d916df90f40c8
uribinnun <118348404+uribinnun@users.noreply.github.com>
2022-11-17 12:55:03 +0200

    Update nouns.py

--- nouns.py
+++ nouns.py
@@ -18,8 +18,6 @@ def first_declention(noun, case, number):
         return nouns_pl['acc']
 
 
-
-
 print(first_declention(noun=input("choose a noun from the first declention: "),
                        case=input("choose a case: 1-nom, 2-gen, 3-dat, 4-acc, 5-abl, 6-voc: "),
                        number=input('1-singular 2-plural: ')))
